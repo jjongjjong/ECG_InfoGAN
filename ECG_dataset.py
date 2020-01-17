d3202a653062f6ef372f6c6123b5120476c67d6b
jjong <jangood1122@ajou.ac.kr>
2020-01-17 21:48:35 +0900

    utils

--- ECG_dataset.py
+++ ECG_dataset.py
@@ -13,18 +13,24 @@ class ECG_dataset (Dataset):
         self.sample_step = sample_step
         self.length = length
         self.norm = norm
+        print('origin {}hz, sample step {}, output {}hz, data len {}, norm {}'.format(freq,sample_step,freq/sample_step,length,norm))
 
     def __len__(self):
         return len(self.filepathList)
 
     def __getitem__(self, idx):
-        ecg_arr = pickle.load(open(self.filepathList,'rb'))
+        ecg_arr = pickle.load(open(self.filepathList[idx],'rb'))
         ecg_mean = 0
         ecg_std = 1
         if self.norm:
             ecg_mean = ecg_arr.mean(keepdims=True)
             ecg_std = ecg_arr.std(keepdims=True)
             ecg_arr = ecg_arr - ecg_mean/ecg_std
+
+        ecg_arr = ecg_arr[::self.sample_step]
+
+        assert len(ecg_arr<=self.length), print('ecg_arr[{}] is shorten than the length [{}]'.format(len(ecg_arr,self.length)))
+        ecg_arr = ecg_arr[:self.length]
 
         return torch.from_numpy(ecg_arr),torch.tensor(ecg_mean[0]),torch.tensor(ecg_std[0])
 
